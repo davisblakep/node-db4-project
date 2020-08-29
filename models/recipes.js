a2443f9ddacb7e6b603225c3e333d645ef3983f1
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-08-28 20:49:36 -0400

    Crud operations made for Recipes and Ingredients

--- models/recipes.js
+++ models/recipes.js
@@ -1,7 +1,11 @@
 const db = require("../data/config");
 
 function getRecipes() {
-  return db("recipes").select("recipes.id", "recipes.recipe");
+  return db("recipes").select(
+    "recipes.id",
+    "recipes.recipe",
+    "recipes.instructions"
+  );
 }
 
 function getRecipesById(id) {
@@ -16,15 +20,29 @@ function getShoppingList(id) {
     .where("recipes.id", id)
     .join("recipes", "recipes.id", "recipe_ingredients.recipe_id")
     .join("ingredients", "ingredients.id", "recipe_ingredients.ingredient_id")
-    .select("recipe", "ingredient");
+    .select("ingredients.id as ingredient_id", "ingredient");
   // .first();
 }
 
 function getInstructions(id) {
   return db("recipes")
-    .select("recipes.id", "recipes.recipe", "recipes.instructions")
+    .select("recipes.id", "recipes.instructions")
     .where("recipes.id", id)
     .first();
+}
+
+function addRecipe(recipe) {
+  return db("recipes")
+    .insert(recipe, "id")
+    .then(([id]) => getRecipesById(id));
+}
+
+function updateRecipe(changes, id) {
+  return db("recipes").where({ id }).update(changes);
+}
+
+function deleteRecipe(id) {
+  return db("recipes").where({ id }).delete();
 }
 
 module.exports = {
@@ -32,4 +50,7 @@ module.exports = {
   getRecipesById,
   getShoppingList,
   getInstructions,
+  addRecipe,
+  updateRecipe,
+  deleteRecipe,
 };
--- routers/recipe.js
+++ routers/recipe.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Recipe = require("../models/recipes");
+const recipes = require("../models/recipes");
 
 const router = express.Router();
 
@@ -52,6 +53,53 @@ router.get("/recipes/:id/instructions", async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+});
+
+router.post("/recipes", (req, res) => {
+  const recipeData = req.body;
+
+  Recipe.addRecipe(recipeData)
+    .then((recipe) => {
+      res.status(201).json(recipe);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to create a new recipe." });
+    });
+});
+
+router.put("/recipes/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  Recipe.getRecipesById(id)
+    .then((x) => {
+      if (x) {
+        Recipe.updateRecipe(changes, id).then((updatedScheme) => {
+          res.json(updatedScheme);
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find scheme with given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to update scheme" });
+    });
+});
+
+router.delete("/recipes/:id", (req, res) => {
+  const { id } = req.params;
+
+  Recipe.deleteRecipe(id).then((deleted) => {
+    if (deleted) {
+      res.json({ message: "Deleted.  Game Over" });
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find the recipe with the given id." });
+    }
+  });
 });
 
 module.exports = router;
